b3ad89514b8a4def14c2f8fb4f99a6f474123225
Alan Flores <alanif@hotmail.com>
2022-10-07 23:22:05 -0500

    Version 1.0.5 refactor: remove { from the emojis spans elements

--- js/components/cardComida.js
+++ js/components/cardComida.js
@@ -5,24 +5,24 @@ const CardComida = (title,emoji1,emoji2,emoji3) =>{
     <div class = "cardComida__container">
         <h2 class="cardComida__container-title">${title}</h2>
         <div class = "cardComida__container-Comida">
-            <span class = "cardComida__container-Comida-emojis">${emoji1}{</span>
-            <span class = "cardComida__container-Comida-emojis">${emoji2}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji1}</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji2}</span>
             <span class = "cardComida__container-Comida-emojis">${emoji3}</span>
         </div>
     </div>
     <div class = "cardComida__container">
         <h2 class="cardComida__container-title">${title}</h2>
         <div class = "cardComida__container-Comida">
-            <span class = "cardComida__container-Comida-emojis">${emoji1}{</span>
-            <span class = "cardComida__container-Comida-emojis">${emoji2}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji1}</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji2}</span>
             <span class = "cardComida__container-Comida-emojis">${emoji3}</span>
         </div>
     </div>
     <div class = "cardComida__container">
         <h2 class="cardComida__container-title">${title}</h2>
         <div class = "cardComida__container-Comida">
-            <span class = "cardComida__container-Comida-emojis">${emoji1}{</span>
-            <span class = "cardComida__container-Comida-emojis">${emoji2}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji1}</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji2}</span>
             <span class = "cardComida__container-Comida-emojis">${emoji3}</span>
         </div>
     </div>
