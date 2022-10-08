c3f2c39569e204526de932e33ab906fa26c48fe5
Moreno-R01 <OscarMR01@outlook.com>
2022-10-07 22:59:57 -0500

    Version 1.0.3 Se añadio los estilos base de las cards de las comidas Se añadio el componente de los elementos de las comidas

---
+++ js/components/cardComida.js
@@ -0,0 +1,34 @@
+const CardComida = (title,emoji1,emoji2,emoji3) =>{
+    const $div = document.createElement("div")
+    $div.className ="cardComida"
+    $div.innerHTML =`
+    <div class = "cardComida__container">
+        <h2 class="cardComida__container-title">${title}</h2>
+        <div class = "cardComida__container-Comida">
+            <span class = "cardComida__container-Comida-emojis">${emoji1}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji2}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji3}</span>
+        </div>
+    </div>
+    <div class = "cardComida__container">
+        <h2 class="cardComida__container-title">${title}</h2>
+        <div class = "cardComida__container-Comida">
+            <span class = "cardComida__container-Comida-emojis">${emoji1}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji2}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji3}</span>
+        </div>
+    </div>
+    <div class = "cardComida__container">
+        <h2 class="cardComida__container-title">${title}</h2>
+        <div class = "cardComida__container-Comida">
+            <span class = "cardComida__container-Comida-emojis">${emoji1}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji2}{</span>
+            <span class = "cardComida__container-Comida-emojis">${emoji3}</span>
+        </div>
+    </div>
+    `
+
+    return $div;
+
+}
+export default CardComida
--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ import '../css/style.css';
 import javascriptLogo from '../public/javascript.svg'
 import { setupCounter } from './counter'
 import Card from "./components/cardMovie.js"
+import CardComida from "./components/cardComida.js"
 
-document.querySelector('#app').appendChild(Card("Estrenos","https://ver.cuevana.pro/resize/200/storage/52637/Yia0FAkvqOBMpvi83TtKxbHTM2ZDnnajBUvH3RRz.jpg","https://ver.cuevana.pro/resize/200/storage/54198/WWn29ij6eEbhrgYsJcv6nehNoI4H9Zb0Zx7suhhz.jpg","https://ver.cuevana.pro/resize/200/storage/54212/iyqPgWDBPXyu43p3X3XSxRVJXBQLQ0j8ZRouSH2o.jpg"))
+document.querySelector('#app').appendChild(Card("Estrenos","https: //ver.cuevana.pro/resize/200/storage/52637/Yia0FAkvqOBMpvi83TtKxbHTM2ZDnnajBUvH3RRz.jpg","https://ver.cuevana.pro/resize/200/storage/54198/WWn29ij6eEbhrgYsJcv6nehNoI4H9Zb0Zx7suhhz.jpg","https://ver.cuevana.pro/resize/200/storage/54212/iyqPgWDBPXyu43p3X3XSxRVJXBQLQ0j8ZRouSH2o.jpg"))
+document.querySelector('#app').appendChild(CardComida("Comboloco", "🍔","🍟","🥤"))
 document.querySelector('#app').appendChild(Card("Preventa"))
+
 
